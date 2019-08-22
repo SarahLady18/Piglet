064145c4ce26d02b817f6b1847a1305033694753
SarahLady18 <54377821+SarahLady18@users.noreply.github.com>
2019-08-22 18:23:50 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,10 @@
+//Javascript pizzaFunctions.js//
+//S. Tobicash 8/22/19 //
+
+function setRed(){
+	pageTitle.style.background = "tomato";
+}
+
+function setWhite(){
+	pageTitle.style.background = "white";
+}
